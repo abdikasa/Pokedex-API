c6b4de3b518d9af3b0e60265866752a64928e896
Said-Abdikarim <kareemfrmto@gmail.com>
2019-09-24 22:16:18 -0400

    Added functionality to the numbered button list on the right and the search bar
    Added functionality to the numbered button list on the right and the search bar to dynamically change the static page into  a different static pokemon page with the number the user's picked

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
 const interface = new UI();
-const poke = new Pokedata(125);
+const poke = new Pokedata("151");
 
 document.addEventListener('DOMContentLoaded', function () {
-    getPokemon()
-
-    // //Add the animation to the pokemon div.
+    //Add the animation to the pokemon div.
     // interface.pokemon.classList.toggle('animate-gengar');
-
-    // //When animation is finished, delete the container
+    getPokemon();
+    //When animation is finished, delete the container
     // document.addEventListener('animationend', function () {
     //     interface.removeLoader().then((resolve) => {
     //         resolve.remove();
+    //         getPokemon();
     //     });
     // })
 })
 
+Array.from(document.querySelector('.pkmn-search').children).forEach((item) => {
+    item.addEventListener("keypress", (e) => {
+        if (e.which === 13 || e.keyCode === 13) {
+            //Check to see if output is correct.
+            let type = poke.checkPokemonIDLW151(item.value);
+            console.log(type);
+            if (type === true || type === "string") {
+                interface.clearUI();
+                poke.changePokemon(item.value);
+                getPokemon();
+            }
+            Array.from((document.querySelector('.pkmn-search').children)).forEach((item) => {
+                return clearInputs(item);
+            });
+        }
+    })
+})
+
+document.querySelector('.vertical-pokedex').addEventListener('click', (e) => {
+    let curr = document.querySelector('.selected-num');
+    if (e.target.parentElement.id === "prev-btn") {
+        console.log("prev")
+        interface.clearUI();
+        poke.changePokemon(`${Number(curr.textContent) - Number("1")}`);
+        getPokemon();
+    } else if (e.target.parentElement.id === "next-btn") {
+        interface.clearUI();
+        poke.changePokemon(`${Number(curr.textContent) + Number("1")}`);
+        getPokemon();
+    } else {
+
+    }
+})
+
+function clearInputs(input) {
+    return input.value = '';
+}
 
 function getPokemon() {
     poke.fetchPokemon()
@@ -22,7 +58,6 @@ function getPokemon() {
             console.log(resolve.pokePromise);
             console.log(resolve.speciesPromise);
             interface.paintUI(resolve.pokePromise, resolve.speciesPromise);
-
         })
         .catch(reject => {
             console.warn(reject)
