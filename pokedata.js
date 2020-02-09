2c2f33f4a3b13e8c31c9a5cd884fefd986c6f8b8
Said-Abdikarim <kareemfrmto@gmail.com>
2020-02-08 22:58:17 -0500

    Attempting to refactor the code using separate functions and trying to make it cleaner overall
    Code looked awful but ran. Was also far too small, over 1 second to fetch a pokemon is too slow and it looked ugly in between fetching, which is why i added the loader.
    
    We are no longer just awaiting each statement, now that I have a better grasp of promises and async/await I'll be using them more often when necessary.

--- pokedata.js
+++ pokedata.js
@@ -2,40 +2,42 @@ class Pokedata {
     constructor(id_name) {
         this.id_name = id_name;
     }
-
+  
     async fetchPokemon() {
-        // First we retreive the pokemon, then the species in order to see its evolutionary chain.
-
-        //pokePromise contains abilities, id, name, sprites, stats, type, height/weight.
-        const pokeResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.id_name}/`);
-        const pokePromise = await pokeResponse.json();
-
-        //speciesPromise contains information about the color and kanji. 
-        const speciesResponse = await fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.id_name}/`);
-        const speciesPromise = await speciesResponse.json();
-
+       
+        const pokeResponse = fetch(`https://pokeapi.co/api/v2/pokemon/${this.id_name}/`);
+        const speciesResponse = fetch(`https://pokeapi.co/api/v2/pokemon-species/${this.id_name}/`);
+        const response = await Promise.all([pokeResponse, speciesResponse])
+        console.log(response);
+        const prom = response.map((test) => {return test.json()});
+  
         return {
-            pokePromise,
-            speciesPromise,
+            pokePromise: await prom[0],
+            speciesPromise: await prom[1]
         }
+       
+        // const urls = [`https://pokeapi.co/api/v2/pokemon/${this.id_name}/`,
+        // `https://pokeapi.co/api/v2/pokemon-species/${this.id_name}/`];
     }
-
-    checkPokemonIDLW151(obj){
-        if(typeof Number(obj) === "number"){
+  
+    checkPokemonIDLW151(obj) {
+        if (typeof Number(obj) === "number") {
             return Number(obj) < 152 && Number(obj) != 0;
-        }else if (typeof Number(obj) === NaN){
+        } else if (typeof Number(obj) === NaN) {
             return "string";
         }
     }
-
+  
     changePokemon(poke) {
         let type = this.checkPokemonIDLW151(poke);
         console.log(type, poke);
-        if(type === true || type === "string"){
+        if (type === true || type === "string") {
             this.id_name = poke;
-        }else{
+        } else {
             console.log("Pokemon entry is greater than 151");
         }
     }
-}
-
+ }
+  
+  
+ 
